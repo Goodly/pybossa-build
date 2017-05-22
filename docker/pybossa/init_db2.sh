ce4dd09ea1f1f6fc7af9a5907c0fe20674db2e7e
Norman Gilmore <git@teamforecast.com>
2017-05-22 14:25:07 -0700

    Change init_db to run 'cli.py db_rebuild' to allow easier resets during testing.

--- docker/pybossa/init_db2.sh
+++ docker/pybossa/init_db2.sh
@@ -2,4 +2,7 @@
 set -e
 cd /home/pybossa/repo
 . /home/pybossa/env/bin/activate
-python cli.py db_create
+# db_create adds two default categories and doesn't reset DB
+# python cli.py db_create
+# Reset DB from scratch
+python cli.py db_rebuild
