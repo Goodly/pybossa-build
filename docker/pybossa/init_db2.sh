73b1ad6bdf4146babe393615a6164e378c972ed2
Norman Gilmore <git@teamforecast.com>
2017-05-23 13:23:16 -0700

    Revert init_db2.sh to use 'db_create' instead of 'db_rebuild'

--- docker/pybossa/init_db2.sh
+++ docker/pybossa/init_db2.sh
@@ -2,7 +2,4 @@
 set -e
 cd /home/pybossa/repo
 . /home/pybossa/env/bin/activate
-# db_create adds two default categories and doesn't reset DB
-# python cli.py db_create
-# Reset DB from scratch
-python cli.py db_rebuild
+python cli.py db_create
