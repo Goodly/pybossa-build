bb6e3e2b4690cf5d30ffa5e9fd383f49d1931e5c
Norman Gilmore <git@teamforecast.com>
2017-07-11 10:04:14 -0700

    Revise database reset to use 'with as' for database connection and cursor.
    Remove incorrect comment about which postgres client version is installed.

--- docker/pybossa/reset_pybossa_db.py
+++ docker/pybossa/reset_pybossa_db.py
@@ -10,25 +10,24 @@ DATABASE_URL = os.environ.get('PYBOSSA_DATABASE_URL',
 
 config = dj_database_url.parse(DATABASE_URL, conn_max_age=60)
 
-conn = psycopg2.connect(host=config['HOST'],
+with psycopg2.connect(host=config['HOST'],
                         port=config['PORT'],
                         dbname='postgres',
                         user=config['USER'],
-                        password=config['PASSWORD'])
-DROP_DB_SQL = """
-    DROP DATABASE IF EXISTS {0};
-""".format(ext.quote_ident(config['NAME'], conn))
-CREATE_DB_SQL = """
-    CREATE DATABASE {0}
-    ENCODING 'UTF-8' LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8'
-    TEMPLATE template0;
-""".format(ext.quote_ident(config['NAME'], conn))
+                        password=config['PASSWORD']) as conn:
+    DROP_DB_SQL = """
+        DROP DATABASE IF EXISTS {0};
+    """.format(ext.quote_ident(config['NAME'], conn))
+    CREATE_DB_SQL = """
+        CREATE DATABASE {0}
+        ENCODING 'UTF-8' LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8'
+        TEMPLATE template0;
+    """.format(ext.quote_ident(config['NAME'], conn))
 
 # autocommit mode required for create and drop
-conn.set_session(autocommit=True)
-cur = conn.cursor()
-print DROP_DB_SQL
-cur.execute(DROP_DB_SQL)
-print CREATE_DB_SQL
-cur.execute(CREATE_DB_SQL)
-conn.close()
+    conn.set_session(autocommit=True)
+    with conn.cursor() as cur:
+        print DROP_DB_SQL
+        cur.execute(DROP_DB_SQL)
+        print CREATE_DB_SQL
+        cur.execute(CREATE_DB_SQL)
